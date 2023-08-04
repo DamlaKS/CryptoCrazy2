2786feefb8409672606481165e4e10457db56860
Damla KS <damlaksrbst@gmail.com>
2023-08-05 00:15:08 +0300

    first commit

---
+++ CryptoCrazy2/Entitiy.swift
@@ -0,0 +1,13 @@
+//
+//  Entitiy.swift
+//  CryptoCrazy2
+//
+//  Created by Damla KS on 4.08.2023.
+//
+
+import Foundation
+
+struct Crypto : Decodable {
+    let currency : String
+    let price : String
+}
